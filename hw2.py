3d575f38e0091da999cef2997e01c3727c31fcb0
Junil-Kwon <kjil0423@gmail.com>
2024-03-25 17:29:30 +0900

    팀 이름 추가됨

--- hw2.py
+++ hw2.py
@@ -1,6 +1,8 @@
 name = input("이름을 입력하세요: ")
 studentId = input("학번을 입력하세요: ")
+teamName = input("팀 이름을 입력하세요: ")
 
 print("\n<출력>")
 print("이름: {}".format(name))
 print("학번: {}".format(studentId))
+print("팀 이름: {}".format(teamName))
